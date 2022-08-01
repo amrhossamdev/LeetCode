9b2cec840efa3268c493ca7eaaa9381f8c6b8c7b
Amr Hossam <amrhossam.dev@gmail.com>
2022-08-01 04:35:58 +0200

    Time: 465 ms (42.32%), Space: 94.6 MB (25.15%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,31 +1,24 @@
 class Solution {
  public:
   bool checkValid(vector<vector<int>>& v) {
-    map<int, int> mp;
     int n = v[0].size();
     for (int i = 0; i < n; ++i) {
+      map<int, int> mp;
       for (int j = 0; j < n; ++j) {
         mp[v[i][j]]++;
       }
       if (mp.size() != n) {
         return false;
       }
-      for (auto i : mp) {
-        if (i.second != 1) return false;
-      }
-      mp.clear();
     }
     for (int i = 0; i < n; ++i) {
+      map<int, int> mp;
       for (int j = 0; j < n; ++j) {
         mp[v[j][i]]++;
       }
       if (mp.size() != n) {
         return false;
       }
-      for (auto i : mp) {
-        if (i.second != 1) return false;
-      }
-      mp.clear();
     }
     return true;
   }
