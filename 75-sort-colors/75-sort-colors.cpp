9d490f20ecbb816607a1ee2e1236b0d69dc1d879
Amr Hossam <amrhossam.dev@gmail.com>
2022-08-05 18:52:10 +0200

    Time: 0 ms (100.00%), Space: 8.4 MB (23.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& v) {
-        priority_queue<int,vector<int>,greater<int>>pq;
+        int z =0,o=0,t=0;
         int n =v.size();
         for(int i =0;i<n;++i){
-            pq.push(v[i]);
+            if(v[i] ==0)++z;
+            else if(v[i] == 1)++o;
+            else {
+                t++;
+            }
         }
         int i = 0;
-        while(!pq.empty()){
-            v[i++] = pq.top();
-            pq.pop();
+        while(z||o||t){
+            if(z){v[i++]=0;z--;}
+            else if(o){v[i++]=1;o--;}
+            else if(t){v[i++] = 2;t--;}
         }
     }
 };
