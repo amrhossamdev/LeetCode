993f31b882404dd37a179cf90d41e3f4d2538fda
Amr Hossam <amrhossam.dev@gmail.com>
2022-08-05 20:35:23 +0200

    Time: 1133 ms (15.07%), Space: 8.1 MB (37.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -5,11 +5,11 @@ public:
     int n;
     int m;
 
-    bool isValid(int i, int j, int n, int m, vector <vector<char>> &board, int &cnt, string &word) {
+    bool isValid(int i, int j, int n, int m, vector <vector<char>> &board, int cnt, string &word) {
         return (i >= 0 && i < n && j < m && j >= 0 && board[i][j] == word[cnt]);
     }
 
-    bool dfs(int i, int j, vector <vector<char>> &board, int cnt, string &word) {
+    bool dfs(int i, int j, vector <vector<char>> &board, int cnt, string word) {
         if (cnt == word.length())return true;
         if (!isValid(i, j, n, m, board, cnt, word))return false;
         char temp = board[i][j];
@@ -24,6 +24,7 @@ public:
     bool exist(vector <vector<char>> &board, string word) {
         n = board.size();
         m = board[0].size();
+        string ans("");
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (word[0] == board[i][j] && dfs(i, j, board, 0, word)) {
