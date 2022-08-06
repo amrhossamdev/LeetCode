2e1edd184759b3b5b1f1446061380389bd6d232a
Amr Hossam <amrhossam.dev@gmail.com>
2022-08-06 03:43:20 +0200

    Time: 71 ms (78.48%), Space: 33.2 MB (33.80%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -8,17 +8,10 @@ class ParkingSystem {
   }
 
   bool addCar(int v) {
-    if (v == 1 && mp[1] > 0) {
-      mp[1]--;
+    if (mp[v] > 0) {
+      mp[v]--;
       return true;
-    } else if (v == 2 && mp[2] > 0) {
-      mp[2]--;
-      return true;
-    } else if (v == 3 && mp[3] > 0) {
-      mp[3]--;
-      return true;
-    } else {
-      return false;
     }
+    return false;
   }
 };
