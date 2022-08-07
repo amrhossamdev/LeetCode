29bb595a3597985a548793dd2a9c6a81403caeb7
Amr Hossam <amrhossam.dev@gmail.com>
2022-08-07 19:45:31 +0200

    Time: 69 ms (10.34%), Space: 24.8 MB (90.43%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
  public:
   vector<int> decode(vector<int>& encoded, int first) {
-    vector<int> ans(encoded.size() + 1, 0);
+    vector<int> ans(encoded.size() + 1);
     ans[0] = first;
     for (int i = 0; i < encoded.size(); ++i) {
       ans[i + 1] = encoded[i] ^ ans[i];
