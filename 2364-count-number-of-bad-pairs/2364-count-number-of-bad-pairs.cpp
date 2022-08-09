74643c6e72353bf67f059af1684285655b5334d5
Amr Hossam <amrhossam.dev@gmail.com>
2022-08-09 22:44:27 +0200

    Time: 379 ms (25.00%), Space: 85.7 MB (100.00%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& v) {
+        map<long long , int>mp;
+        long long n = v.size();
+        long long p = n*(n-1)/2;//total pairs
+        long long goodpairs=0;
+        for(int i =0;i<v.size();++i){
+            long long cur = i-v[i];
+            goodpairs+=mp[cur];
+            mp[cur]++;
+        }
+        return p-goodpairs;
+    }
+};
