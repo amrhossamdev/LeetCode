339cfc3e0e139f508e38f1d00ce4b72d616ad018
Amr Hossam <amrhossam.dev@gmail.com>
2022-10-25 18:40:25 +0200

    Time: 20 ms (49.58%), Space: 7.3 MB (72.81%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,26 +1,24 @@
 class Solution {
  public:
-
   bool checkInclusion(string s1, string s2) {
-    if(s1.size() > s2.size())return false;
-    vector<int>s1Count(26,0);
-    vector<int>s2Count(26,0);
-    int l = 0,r =0;
-    while(r<s1.size()){
-        s1Count[s1[r]-'a']++;
-        s2Count[s2[r]-'a']++;
-        ++r;
+    if (s1.size() > s2.size()) return false;
+    vector<int> s1Count(26, 0);
+    vector<int> s2Count(26, 0);
+    int l = 0, r = s1.size()-1;
+    for (int i = 0; i < s1.size(); ++i) {
+      s1Count[s1[i] - 'a']++;
+      s2Count[s2[i] - 'a']++;
     }
-    r-=1;
-    while(r < s2.size()){
-        if(s1Count == s2Count)return true;
-        r++;
-        if(r != s2.size()){
-            s2Count[s2[r]-'a']++;
-        }
-        s2Count[s2[l]-'a']--;
-        l++;
-    }   
+
+    while (r < s2.size()) {
+      if (s1Count == s2Count) return true;
+      r++;
+      if (r != s2.size()) {
+        s2Count[s2[r] - 'a']++;
+      }
+      s2Count[s2[l] - 'a']--;
+      l++;
+    }
     return false;
   }
 };
