306d8d9291c9e18b8e20a4e2cfb18bc9eda186e6
Amr Hossam <amrhossam.dev@gmail.com>
2022-10-28 19:21:28 +0200

    Time: 113 ms (41.29%), Space: 19 MB (8.99%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    long long count(long long mid,vector<int>&v,int h){
+        long long sum = 0;
+        for(int i=0;i<v.size();++i){
+                sum+=(ceil(1.0*v[i]/mid));
+            }
+        return sum;
+    }
+
+    int minEatingSpeed(vector<int> &v, int h) {
+     
+        long long l = 1, r = *max_element(v.begin(),v.end());
+        long long ans =r;
+        while (l <= r) {
+            long long sum = 0;
+            long long m = (l+r)/2;
+            if(count(m,v,h)<=h){
+                ans = min(ans,m);
+                r = m - 1;
+            }else {
+                l = m + 1;
+            }
+        }
+        return ans;
+    }
+};
