21e4c3293aaf717924666c810bb4d9cad18956cc
Amr Hossam <amrhossam.dev@gmail.com>
2022-12-25 00:11:45 +0200

    Time: 0 ms (100.00%), Space: 12.2 MB (37.60%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        int i =0;
+        do{
+         if(i==1) break;
+         i++;
+        }while(next_permutation(nums.begin(),nums.end()));
+    }
+};
