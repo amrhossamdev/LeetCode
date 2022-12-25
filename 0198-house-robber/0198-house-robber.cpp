556e42700ded7209751f52eaf5ad511d28d8757d
Amr Hossam <amrhossam.dev@gmail.com>
2022-12-26 00:37:01 +0200

    Time: 5 ms (24.45%), Space: 8 MB (5.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,19 @@
 class Solution {
  public:
   vector<int> dp;
-  int solve(int index,int n, vector<int> v) {
+  int solve(int index, int &n, vector<int> &v) {
     if (index >= n) {
       return 0;
     }
     if (~dp[index]) return dp[index];
     int take = 0, leave = 0;
-    take = v[index] + solve(index + 2,n, v);
-    leave = solve(index + 1, n,v);
+    take = v[index] + solve(index + 2, n, v);
+    leave = solve(index + 1, n, v);
     return dp[index] = max(take, leave);
   }
-  int rob(vector<int>& v) {
+  int rob(vector<int> &v) {
     dp.assign(1000, -1);
-    return solve(0,(int)v.size(), v);
+    int n = v.size();
+    return solve(0,n, v);
   }
 };
