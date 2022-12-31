2fde5108454bdd35688a67c8283fb16f2a2c89a0
Amr Hossam <amrhossam.dev@gmail.com>
2022-12-31 03:21:08 +0200

    Time: 45 ms (46.22%), Space: 9.1 MB (11.59%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -1,16 +1,23 @@
 class Solution {
  public:
+  int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+  }
   int subarrayGCD(vector<int>& v, int k) {
-      int n = v.size();
-      int ans =0;
-      for(int i =0;i<n;++i){
-          int res = v[i];
-          for(int j = i;j<n;++j){
-              res = __gcd(res,v[j]);
-              if(res != k)continue;
-              else{ans++;}
-          }
+    int n = v.size();
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      int res = v[i];
+      for (int j = i; j < n; ++j) {
+        res = gcd(res, v[j]);
+        if (res != k)
+          continue;
+        else {
+          ans++;
+        }
       }
+    }
 
     return ans;
   }
