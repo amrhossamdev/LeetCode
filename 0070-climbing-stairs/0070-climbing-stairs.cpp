64f115e4af7e6d18bfe4d4aebb9bee090fcf1857
Amr Hossam <amrhossam.dev@gmail.com>
2023-01-09 00:14:09 +0200

    Time: 0 ms (100.00%), Space: 6 MB (68.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[50];
+        memset(dp,-1,sizeof(dp));
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2;i<n;++i){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+};
