470cfd2103252b343a484ff0ea16919f64e0f1ba
Amr Hossam <amrhossam.dev@gmail.com>
2023-01-10 19:42:25 +0200

    Time: 4 ms (39.90%), Space: 10.4 MB (8.85%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  vector<int> tree1, tree2;
+  int nodeR1 = 0, nodeR2 = 0, nodeL1 = 0, nodeL2;
+  void preorder(TreeNode* node, bool isTree1) {
+    if (node == nullptr) {
+      return;
+    }
+    if (node->left != nullptr) {
+      if (isTree1) {
+        nodeL1++;
+      } else {
+        nodeL2++;
+      }
+      preorder(node->left, isTree1);
+    }
+    if (node->right != nullptr) {
+      if (isTree1) {
+        nodeR1++;
+      } else {
+        nodeR2++;
+      }
+      preorder(node->right, isTree1);
+    }
+    if (isTree1)
+      tree1.push_back(node->val);
+    else {
+      tree2.push_back(node->val);
+    }
+  }
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    preorder(p, true);
+    preorder(q, false);
+    int n = tree1.size();
+    int m = tree2.size();
+    if (n != m || nodeR1 != nodeR2 || nodeL1 != nodeL2) return false;
+    for (int i = 0; i < n; ++i) {
+      cout << tree1[i] << " " << tree2[i] << endl;
+      if (tree1[i] != tree2[i]) return false;
+    }
+    return true;
+  }
+};
