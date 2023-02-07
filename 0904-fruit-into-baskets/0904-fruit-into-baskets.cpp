da3d54e57c19f1bcf126a404b93744dda9d75395
Amr Hossam <amrhossam.dev@gmail.com>
2023-02-07 14:15:32 +0200

    Time: 196 ms (52.41%), Space: 71.2 MB (48.83%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -6,13 +6,14 @@ class Solution {
     int l = 0, r = 0;
     int n = v.size();
     while (r < n) {
-      mp[v[r++]]++;
+      mp[v[r]]++;
+      r++;
       while (mp.size() > 2) {
         mp[v[l]]--;
         if (mp[v[l]] == 0) mp.erase(v[l]);
         l++;
       }
-      ans = max(ans, r - l);
+      ans = max(r - l, ans);
     }
     return ans;
   }
