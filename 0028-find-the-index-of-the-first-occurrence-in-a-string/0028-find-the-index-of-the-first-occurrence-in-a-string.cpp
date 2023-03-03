5e067e6fd71f39ca6273b58c9cfcfb48d9a47117
Amr Hossam <amrhossam.dev@gmail.com>
2023-03-03 09:36:57 +0200

    Time: 4 ms (38.84%), Space: 6.2 MB (69.99%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int strStr(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        int pos = s1.find(s2);
+        return pos;
+    }
+};
