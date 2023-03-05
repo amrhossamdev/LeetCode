00006cebf9b03170210dd61ad92e75975fc67501
Amr Hossam <amrhossam.dev@gmail.com>
2023-03-05 02:24:59 +0200

    Time: 1598 ms (6.40%), Space: 39.6 MB (25.26%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[2550][2550];
+    int go(int idx,int prev,int &n,vector<int>&v){
+        if(idx >=n){
+            if(idx == n)return 0;
+            return -2e4;
+        }
+        int &ret =dp[idx][prev + 1];
+        if(~ret) return ret;
+        if(prev == -1 || v[prev] < v[idx]){
+          ret = 1 + go(idx + 1,idx,n,v);
+        }
+        ret = max(ret,go(idx + 1,prev,n,v));
+        return ret;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        memset(dp,-1,sizeof(dp));
+        int ans = go(0,-1,n,nums);
+        return ans;
+    }
+};
