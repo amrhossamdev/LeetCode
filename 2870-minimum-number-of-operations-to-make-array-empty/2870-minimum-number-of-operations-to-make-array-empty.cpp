746dd849822fa366f9779054ac50595991b21cfb
Amr Hossam <amrhossam.dev@gmail.com>
2024-01-04 15:35:03 +0200

    Time: 121 ms (58.01%), Space: 85.6 MB (14.00%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -9,23 +9,21 @@ class Solution {
 
     int ans = 0;
     for (auto i : mp) {
-      if (i.second == 1) return -1;
-      int tmp = i.second;
-        cout << i.first << " " << i.second << endl;
-      if (tmp > 4) {
-        if (tmp % 3 == 2) {
-          ans += tmp / 3;
-          tmp -= (tmp / 3) * 3;
-        } else if (tmp % 3 == 1) {
-          ans += (tmp / 3) - 1;
-          tmp -= ((tmp / 3) - 1) * 3;
-        }else{
-            ans+=tmp/3;
-            tmp = 0;
+      int freq = i.second;
+      if (freq == 1) return -1;
+      if (freq > 4) {
+        if (freq % 3 == 2) {
+          ans += freq / 3;
+          freq -= (freq / 3) * 3;
+        } else if (freq % 3 == 1) {
+          ans += (freq / 3) - 1;
+          freq -= ((freq / 3) - 1) * 3;
+        } else {
+          ans += freq / 3;
+          freq = 0;
         }
       }
-        ans += tmp / 2;
-      
+      ans += freq / 2;
     }
     return ans;
   }
