eece30f0e2eecc500aac3bb62163731788dd8978
Amr Hossam <amrhossam.dev@gmail.com>
2024-01-13 05:33:06 +0200

    Time: 104 ms (23.18%), Space: 17.4 MB (6.88%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,15 +1,24 @@
 class Solution {
-public:
-    int minSteps(string s, string t) {
-        vector<int>freq1(26,0),freq2(26,0);
-        for(int i = 0;i<s.size();++i){
-            freq1[s[i]-'a']++;
-            freq2[t[i]-'a']++;
-        }
-        int diff = 0;
-        for(int i = 0;i<26;++i){
-            diff+=abs(freq1[i]-freq2[i]);
-        }
-        return diff/2;
+ public:
+  int minSteps(string s, string t) {
+    map<char, int> mp1, mp2;
+    int n = s.size();
+    for (int i = 0; i < n; ++i) {
+      mp2[t[i]]++;
+      mp1[s[i]]++;
     }
+    int ans = 0;
+    for (char c = 'a'; c <= 'z'; ++c) {
+      if (mp2[c] < mp1[c]) {
+        for (char k = 'a'; k <= 'z'; ++k) {
+          if (mp2[k] > mp1[k]) {
+            ans += mp2[k] - mp1[k];
+            mp2[k] -= (mp2[k] - mp1[k]);
+            mp2[c] += (mp2[k] - mp1[k]);
+          }
+        }
+      }
+    }
+    return ans;
+  }
 };
