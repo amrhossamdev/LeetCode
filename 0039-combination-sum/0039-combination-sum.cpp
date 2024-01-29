520612559486764605ea450f74ad58cfbba666a5
Amr Hossam <amrhossam.dev@gmail.com>
2024-01-29 17:17:47 +0200

    Time: 9 ms (43.63%), Space: 12.4 MB (46.42%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,33 +1,26 @@
 class Solution {
  public:
   vector<vector<int>> ans;
-  map<vector<int>, bool> mp;
-  void rec(int idx, int n, int sum, vector<int> &tmp, vector<int>& v,
+
+  void rec(int idx, int n, int sum, vector<int>& tmp, vector<int>& v,
            int target) {
     if (sum > target) {
       return;
     }
     if (idx >= n) {
       if (sum == target) {
-        if (!mp[tmp]) ans.push_back(tmp);
-        mp[tmp] = 1;
+        ans.push_back(tmp);
       }
       return;
     }
     if (sum == target) {
-      if (!mp[tmp]) ans.push_back(tmp);
-      mp[tmp] = 1;
+      ans.push_back(tmp);
+      return;
     }
     tmp.push_back(v[idx]);
     rec(idx, n, sum + v[idx], tmp, v, target);
     tmp.pop_back();
-
     rec(idx + 1, n, sum, tmp, v, target);
-    if (idx + 1 < n) {
-      tmp.push_back(v[idx]);
-      rec(idx + 1, n, sum + v[idx], tmp, v, target);
-      tmp.pop_back();
-    }
   }
   vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
     vector<int> tmp;
