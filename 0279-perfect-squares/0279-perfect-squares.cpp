e927e7f64460298ace4c400d210a08057710fb94
Amr Hossam <amrhossam.dev@gmail.com>
2024-02-08 13:34:08 +0200

    Time: 87 ms (67.66%), Space: 12.1 MB (17.06%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,24 @@
+class Solution {
+ public:
+  vector<int> v;
+
+  
+  int go(int sum,vector<int> &dp) {
+    if(sum==0)return 0;
+    int &ret = dp[sum];
+    if (~ret) return ret;
+    ret = 1e9;
+    for(int i =1;i*i<=sum;++i){
+    	ret = min(ret,1+ go(sum-(i*i),dp));
+    }
+    return ret;
+  }
+  int numSquares(int n) {
+    for (int i = 1; i <= 100; ++i) {
+      v.push_back(i * i);
+    }
+    vector<int>dp(n + 1,-1);
+    int ans = go(n,dp);
+    return ans;
+  }
+};
